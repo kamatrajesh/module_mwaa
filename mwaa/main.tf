bccdf720a4ddd43807f82f5c80d06160f33cef9a
kamatrajesh <104766746+kamatrajesh@users.noreply.github.com>
2022-05-09 20:08:35 +0530

    Create main.tf

---
+++ mwaa/main.tf
@@ -0,0 +1,42 @@
+resource "aws_mwaa_environment" "example" {
+  dag_s3_path        = "dag/"
+  execution_role_arn = var.execution_role_arn
+  
+
+  logging_configuration {
+    dag_processing_logs {
+      enabled   = true
+      log_level = "INFO"
+    }
+
+    scheduler_logs {
+      enabled   = true
+      log_level = "INFO"
+    }
+
+    task_logs {
+      enabled   = true
+      log_level = "INFO"
+    }
+
+    webserver_logs {
+      enabled   = true
+      log_level = "INFO"
+    }
+
+    worker_logs {
+      enabled   = true
+      log_level = "INFO"
+    }
+  }
+
+  name = var.name
+
+  network_configuration {
+    security_group_ids = var.security_group_ids
+    subnet_ids         = var.subnet_ids
+  }
+
+  source_bucket_arn = var.source_bucket_arn
+  tags =  var.resource_tags
+}
