6c6d0479fa56cb82a8dcc04693fdde464c37ba80
kamatrajesh <104766746+kamatrajesh@users.noreply.github.com>
2022-05-10 22:27:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,19 @@ resource_tags = {
 }
   
 }
+  module "dev_mwaa1" {
+  source="./mwaa"
+  name="raj122"
+security_group_ids=["sg-0068dfad55ea04930"]
+subnet_ids=["subnet-083f60308cc643012","subnet-05244b86b15d97348"]
+execution_role_arn="arn:aws:iam::863309687429:role/mwaa-first-execution-role"
+source_bucket_arn="arn:aws:s3:::airflow197805"
+resource_tags = {
+  Name      = "raj1233",
+  environment = "dev",
   
+}
+  
+}
  
 
